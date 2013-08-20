3090c33686a06013a437a41bca1db71d086de22e
Alvin Kerber <akerber47@gmail.com>
2013-08-20 12:25:17 -0700

    Wrote consistency fxns for paths and maps
    Also refactored some code to use guard (for Maybe)

--- train.hs
+++ train.hs
@@ -9,15 +9,23 @@ import qualified Control.Monad as Monad
 import qualified Data.Graph as Graph
 import Control.Exception.Base (assert)
 
+-- Some really general things we use:
+
+-- Yields the result of applying f until a fixed point is reached.
+untilFixed :: (Eq a) => (a -> a) -> a -> a
+untilFixed f x = fst . head . filter (uncurry (==)) $
+        zip (iterate f x) (tail $ iterate f x)
+
 -- A SpineGraph represents a graph which is embedded as the spine of a surface,
 -- keeping track of the data necessary to:
 -- (1) Implement the Bestvina-Handel algorithm
 -- (2) Display the resulting fibered surface / train track
--- As in Toby Hall's program "Trains", we need to store data on each vertex (the
--- zone of the surface it's located in, and the cyclic order of its incident
--- edges) and each edge (its endpoints, and the zones of the surface it
--- traverses). The surface is divided into "zones" around punctures / boundaries
--- / etc.
+
+-- As in Toby Hall's program "Trains", we need to store data on each vertex
+-- (the zone of the surface it's located in, and the cyclic order of its
+-- incident edges) and each edge (its endpoints, and the zones of the surface
+-- it traverses). The surface is divided into "zones" around punctures /
+-- boundaries / etc.
 -- We represent the graph as an adjacency list AND an incidence list. Any
 -- updates to the graph need to keep all these components in sync.
 -- [BH] G
@@ -44,9 +52,9 @@ data SpineVertex = SpineVertex
     , incidentDirs  :: [Dir] -- Fwd if this vertex is 1st vertex of the edge,
                              -- Back if 2nd vertex
     , vertexZone    :: ZoneID }
-    deriving (Show)
--- If an edge has both endpoints at a single vertex, the edge is listed twice in
--- the incident edge list (once Fwd and once Back)
+    deriving (Show,Eq)
+-- If an edge has both endpoints at a single vertex, the edge is listed twice
+-- in the incident edge list (once Fwd and once Back)
 
 -- Note that edges are undirected. Each edge ID will be listed among the
 -- incident edges of both its start and end vertices. BUT when it comes to
@@ -56,12 +64,12 @@ data SpineEdge = SpineEdge
     { firstVertex    :: VertexID
     , secondVertex   :: VertexID
     , traversedZones :: [ZoneID] }
-    deriving (Show)
+    deriving (Show,Eq)
 
 data SpineGraph = SpineGraph
     { vertexData :: M.Map VertexID SpineVertex
     , edgeData   :: M.Map EdgeID SpineEdge }
-    deriving (Show)
+    deriving (Show,Eq)
 
 -- Directed edge = edge + direction to traverse in
 data DEdge = DEdge EdgeID Dir
@@ -91,20 +99,19 @@ isConsistent (SpineGraph vdata edata) = Maybe.isJust $ do
                -- Check that both endpoints of each edge exist...
                SpineVertex _ _ z1 <- M.lookup v1id vdata
                SpineVertex _ _ z2 <- M.lookup v2id vdata
-               -- ... and have the same zones as the start/end zones of the edge
+               -- ... and have the same zones as start/end zones of the edge
                ez1 <- Maybe.listToMaybe zs
-               if z1 == ez1 && z2 == last zs then Just () else Nothing
+               guard $ z1 == ez1 && z2 == last zs
            checkIncEdges :: VertexID -> Maybe ()
            checkIncEdges vid = do
                SpineVertex ies dirs _ <- M.lookup vid vdata
-               Monad.zipWithM_ (\eid dir -> do
+               Monad.zipWithM_ checkIncEdge ies dirs
+               where checkIncEdge eid dir = do
                    -- Check that all incident edges of each vertex exist...
                    SpineEdge v1id v2id _ <- M.lookup eid edata
                    -- ... and have that vertex as a start or end vertex
-                   if (v1id == vid && dir == Fwd) ||
-                      (v2id == vid && dir == Back) then Just () else Nothing)
-                               ies
-                               dirs
+                   guard $ (v1id == vid && dir == Fwd) ||
+                           (v2id == vid && dir == Back)
 
 -- Convert a SpineGraph into a Data.Graph.Graph by forgetting lots of stuff. We
 -- produce a directed graph with 2 edges for each undirected edge in the
@@ -113,7 +120,7 @@ toAbstractGraph :: SpineGraph -> Graph.Graph
 toAbstractGraph (SpineGraph vdata edata) = Graph.buildG (minvid, maxvid) es
     where es   = (map (\(SpineEdge (VertexID v1) (VertexID v2) _) -> (v1,v2)) $
               M.elems edata) ++
-                  (map (\(SpineEdge (VertexID v1) (VertexID v2) _) -> (v2,v1)) $
+                 (map (\(SpineEdge (VertexID v1) (VertexID v2) _) -> (v2,v1)) $
               M.elems edata)
           vids = map vertexID $ M.keys vdata
           minvid = minimum vids
@@ -121,7 +128,26 @@ toAbstractGraph (SpineGraph vdata edata) = Graph.buildG (minvid, maxvid) es
 
 -- Represents a path of edges. For each edge we indicate whether we traverse it
 -- forwards or backwards.
+-- Note that an edge can map to the empty path, in which case we need to look
+-- up its endpoints to figure out what vertex it's actually "sitting on"
 type Path = [DEdge]
+
+pathStart :: Path -> Maybe VertexID
+pathStart [] = Nothing
+pathStart p  = Monad.liftM fst $ dirEndpoints (head p)
+
+pathEnd :: Path -> Maybe VertexID
+pathEnd [] = Nothing
+pathEnd p  = Monad.liftM snd $ dirEndpoints (last p)
+
+isConsistentPath :: Path -> Bool
+isConsistentPath [] = True
+isConsistentPath p  = Maybe.isJust $ zipWithM_ checkPair p (tail p)
+    where checkPair de1 de2 = do
+              -- end of each edge is start of the next
+              (v11,v12) <- dirEndpoints de1
+              (v21,v22) <- dirEndpoints de2
+              guard $ v12 == v21
 
 -- Map sends vertices to vertices, and edges to edge paths.
 -- [BH] g: G -> G
@@ -129,13 +155,37 @@ data GraphMap = GraphMap
     { graphToMap :: SpineGraph
     , vertexMap  :: M.Map VertexID VertexID
     , edgeMap    :: M.Map EdgeID Path }
-    deriving (Show)
+    deriving (Show,Eq)
 
 vertexPreimage :: GraphMap -> VertexID -> [VertexID]
 vertexPreimage (GraphMap _ vmap _) v = M.keys $ M.filter (== v) vmap
 
 isConsistentMap :: GraphMap -> Bool
-isConsistentMap g = Maybe.isJust $ Just () -- TODO XXX
+isConsistentMap (GraphMap sg@(SpineGraph vdata edata) vmap emap) =
+    isConsistent sg &&
+    -- Maps are defined for all vertices / edges
+    M.keysSet vdata == M.keysSet vmap &&
+    M.keysSet edata == M.keysSet emap &&
+    (Maybe.isJust $ do
+        -- Each vertex maps to a well-defined vertex
+        Monad.forM_ (M.elems vmap) (\v -> M.lookup v vdata)
+        -- Each edge maps to a well-defined consistent edge path
+        Monad.forM_ (M.keys emap) checkEdge)
+    where checkEdge e = do
+            (SpineEdge v1 v2 _) <- M.lookup e edata
+            gv1 <- M.lookup v1 vmap
+            gv2 <- M.lookup v2 vmap
+            path <- M.lookup e emap
+            case path of
+                 []        -> guard $ gv1 == gv2
+                 otherwise -> do
+                     p1 <- pathStart path
+                     p2 <- pathEnd path
+                     guard $ gv1 == p1 && gv2 == p2
+            guard $ isConsistentPath path
+
+
+
 
 -- Map manipulation helper functions.
 
@@ -168,18 +218,14 @@ isoVertex de@(DEdge e dir) v (GraphMap sg@(SpineGraph vdata edata) vmap emap)
                                    Back -> M.adjust (++ [DEdge e dir]) ie
 
 -- Isotope the given map by postcomposing with an isotopy "pulling the given
--- edge's start vertex across itself". That is, apply the earlier isotopy to all
--- preimages of the given edge's starting vertex.
+-- edge's start vertex across itself". That is, apply the earlier isotopy to
+-- all preimages of the given edge's starting vertex.
+
 -- Similarly, error if edge not present.
 isoVertexRight :: DEdge -> GraphMap -> GraphMap
 isoVertexRight de g@(GraphMap sg _ _) =
     foldr (isoVertex de) g (vertexPreimage g v)
     where (v,_) = Maybe.fromJust $ dirEndpoints sg de
-
--- Yields the result of applying f until a fixed point is reached.
-untilFixed :: (Eq a) => (a -> a) -> a -> a
-untilFixed f x = fst . head . filter (uncurry (==)) $
-        zip (iterate f x) (tail $ iterate f x)
 
 -- Return a path which does not backtrack, by removing all parts of the path
 -- which double back on themselves.
@@ -195,6 +241,13 @@ deBacktrack = untilFixed deBacktrackPair
           deBacktrackPair (de1@(DEdge e1 d1):de2@(DEdge e2 d2):des)
             | e1 == e2 && d1 == rev d2 = des
             | otherwise                = de1:de2:(deBacktrackPair des)
+
+-- Return all edges which are fixed under the given map.
+invariantEdges :: GraphMap -> [EdgeID]
+invariantEdges (GraphMap _ _ emap) = filter isInv $ M.keys emap
+    where isInv e = [(e,Fwd) == Maybe.fromJust (M.lookup e emap)]
+
+
 
 -- Implementations of the fibered surface moves.
 -- 1. Collapse invariant forest
